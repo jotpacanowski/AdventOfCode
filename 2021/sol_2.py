70b45afbbc82763a91908331a265655c15cc8ffc
Jędrzej Pacanowski <42004734+jotpacanowski@users.noreply.github.com>
2021-12-02 22:34:29 +0100

    Refactor a bit + start 3.10 match-case port

--- 2021/sol_2.py
+++ 2021/sol_2.py
@@ -1,4 +1,5 @@
 # 2021-12-02
+# Python 3.10 port of day 2 solution
 
 def main1(lines):
     fwd = 0
@@ -40,7 +41,9 @@ if __name__ == '__main__':
     inp_values = open('2-input', 'r').read().splitlines()
     inp_values = [(x.split()[0], int(x.split()[1])) for x in inp_values]
     print(f'{len(inp_values)} lines')
+
     answ = main1(inp_values)
     print(f'Answer: {answ} \n Pt. 2')
+
     answ = main2(inp_values)
     print(f'Answer: {answ}')
