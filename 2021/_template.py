a53bb289cceb1d745bcc9c1b312c90f7be6e4b4a
Jędrzej Pacanowski <42004734+jotpacanowski@users.noreply.github.com>
2021-12-05 14:01:28 +0100

    Add template file

---
+++ 2021/_template.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# 2021-12-
+
+import io
+from pprint import pprint
+
+
+def main1(values) -> int:
+    return
+
+
+def main2(values) -> int:
+    return
+
+
+EXAMPLE_IN = io.StringIO("""
+
+""")
+
+if __name__ == '__main__':
+    USE_EXAMPLE_IN = False
+    with EXAMPLE_IN if USE_EXAMPLE_IN else open('-input', 'r') as f:
+        inp_values = f.read().splitlines()
+        inp_values = [int(x) for x in inp_values]
+    print(f'{len(inp_values)} lines')
+
+    answ = main1(inp_values)
+    print(f'Answer: {answ}')
+    # Correct answer for my input:
+
+    answ2 = main2(inp_values)
+    print(f'Part 2, Answer: {answ2}')
+    # Correct answer for my input:
