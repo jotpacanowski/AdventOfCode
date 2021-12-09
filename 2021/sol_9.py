528f156cbe2f53a787d3924324602a9df9ef427c
Jędrzej Pacanowski <42004734+jotpacanowski@users.noreply.github.com>
2021-12-09 20:23:46 +0100

    Solved day 9

--- 2021/sol_9.py
+++ 2021/sol_9.py
@@ -41,7 +41,30 @@ def main1(values) -> int:
 
 
 def main2(values, ls) -> int:
-    return
+    visited = [[0] * len(values[0]) for _ in range(len(values))]
+    basin_sz = []
+
+    def dfs(basin_id, row, col):
+        if visited[row][col] == 1:
+            return
+        visited[row][col] = 1
+        basin_sz[basin_id] += 1
+        if row > 0 and values[row - 1][col] != 9:
+            dfs(basin_id, row - 1, col)
+        if col > 0 and values[row][col - 1] != 9:
+            dfs(basin_id, row, col - 1)
+        if row + 1 < len(values) and values[row + 1][col] != 9:
+            dfs(basin_id, row + 1, col)
+        if col + 1 < len(values[0]) and values[row][col + 1] != 9:
+            dfs(basin_id, row, col + 1)
+
+    for r, c in ls:
+        basin_id = len(basin_sz)
+        basin_sz.append(0)
+        dfs(basin_id, r, c)
+
+    basin_sz.sort(reverse=True)
+    return basin_sz[0] * basin_sz[1] * basin_sz[2]
 
 
 EXAMPLE_IN = io.StringIO("""2199943210
@@ -71,6 +94,8 @@ if __name__ == '__main__':
     print(f'\x1b[32;1mAnswer: {answ} \x1b[0m')
     # Correct answer for my input: 545
 
+    sys.setrecursionlimit(100)
+
     answ2 = main2(inp_values, ls)
     print(f'Part 2,\x1b[32;1m Answer: {answ2} \x1b[0m')
-    # Correct answer for my input:
+    # Correct answer for my input: 950600
