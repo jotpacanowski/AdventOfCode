6f61c04c40519129e5515caf7ae5f84113410954
Jędrzej Pacanowski <42004734+jotpacanowski@users.noreply.github.com>
2021-12-12 09:50:49 +0100

    Solved day 12 part 1

--- 2021/sol_12.py
+++ 2021/sol_12.py
@@ -17,8 +17,34 @@ def edges_to_graph(edges_list):
     return G
 
 
-def main1(values) -> int:
-    return
+def main1(G) -> int:
+    """
+    How many paths through this cave system are there
+    that visit small caves at most once?
+    """
+    total_paths = 0
+    visited = {k: 0 for k in G.keys()}
+    stack = []
+
+    def dfs_visit(node):
+        nonlocal stack, visited, total_paths
+        if node == 'end':
+            stack.append('end')
+            print('New path: ', '-'.join(stack))
+            stack.pop()
+            total_paths += 1
+        stack.append(node)
+        for neigh in G[node]:
+            if neigh.islower() and neigh not in stack:
+                dfs_visit(neigh)
+            elif neigh.isupper():
+                dfs_visit(neigh)
+            else:
+                pass  # raise ValueError(f'node name {node!r}')
+        stack.pop()
+
+    dfs_visit('start')
+    return total_paths
 
 
 def main2(values) -> int:
@@ -44,11 +70,31 @@ kj-sa
 kj-HN
 kj-dc
 """
+EXAMPLE_3 = """fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW
+"""
 
-EXAMPLE_IN = io.StringIO(EXAMPLE_1)
+EXAMPLE_IN = io.StringIO(EXAMPLE_2 if 'ex2' in sys.argv else EXAMPLE_1)
+# EXAMPLE_IN = io.StringIO(EXAMPLE_3)
 
 if __name__ == '__main__':
-    USE_EXAMPLE_IN = 'ex' in sys.argv  # False
+    USE_EXAMPLE_IN = 'ex' in sys.argv or 'ex2' in sys.argv
     if USE_EXAMPLE_IN:
         print('\x1b[31;1m Using EXAMPLE input! \x1b[0m')
     with EXAMPLE_IN if USE_EXAMPLE_IN else open('12-input', 'r') as f:
@@ -57,13 +103,14 @@ if __name__ == '__main__':
     print(f'{len(inp_values)} edges')
 
     G = edges_to_graph(inp_values)
+    print(f'{len(G)} vertices')
     print('  GRAPH: ')
     pprint(G)
 
-    answ = main1(inp_values)
+    answ = main1(G)
     print(f'\x1b[32;1mAnswer: {answ} \x1b[0m')
-    # Correct answer for my input:
+    # Correct answer for my input: 3369
 
-    answ2 = main2(inp_values)
+    answ2 = main2(G)
     print(f'Part 2,\x1b[32;1m Answer: {answ2} \x1b[0m')
     # Correct answer for my input:
