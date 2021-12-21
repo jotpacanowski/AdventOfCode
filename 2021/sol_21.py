888b31a46dc0e64aa3daef89a45a6ab801317fa9
Jędrzej Pacanowski <42004734+jotpacanowski@users.noreply.github.com>
2021-12-21 14:18:25 +0100

    Solved (21) part 1

--- 2021/sol_21.py
+++ 2021/sol_21.py
@@ -14,32 +14,52 @@ def move_pawn(pos: int, steps: int) -> int:
 
 def add_modulo_p1(a: int, b: int, /, mod: int) -> int:
     """Perform addition modulo `mod` of $a+b-1$ and add 1 to the result."""
-    a = (a + b - 1) % mod
-    return a + 1
+    return ((a + b - 1) % mod) + 1
+    # a -= 1
+    # a += b
+    # while a > mod:
+    #     a -= mod
+    # return a + 1
 
 
 def main1(inp_values) -> int:
     p1pos, p2pos = inp_values  # Positions on the board 1..10
-    p1sc, p2sc = 0
-    dice_id = 0
+    p1sc = p2sc = 0
+    dice_id = 1
+    rolled_times = 0
 
     while p1sc < 1000 and p2sc < 1000:
         roll = 3 * dice_id + 3
-        dice_id += 3
+        # print(f'P1 rolls {dice_id},{dice_id+1},{dice_id+2} == {roll} ', end='')
+        dice_id = add_modulo_p1(dice_id, 3, 100)
+        rolled_times += 3
+
         p1pos = move_pawn(p1pos, roll)
         p1sc += p1pos
+        # print(f'and goes to', p1pos, 'w/', p1sc, 'points')
 
         if p1sc >= 1000:
             break
 
         # Same thing
         roll = 3 * dice_id + 3
-        dice_id += 3
+        # print(f'P2 rolls {dice_id},{dice_id+1},{dice_id+2} == {roll} ', end='')
+        dice_id = add_modulo_p1(dice_id, 3, 100)
+        rolled_times += 3
+
         p2pos = move_pawn(p2pos, roll)
         p2sc += p2pos
+        # print(f'and goes to', p2pos, 'w/', p2sc, 'points')
 
+    print(f' * Dice has been rolled {rolled_times} times.')
     if p1sc >= 1000:
-        print('Player 1 won')
+        print(f'-- Player 1 won with {p1sc} points  > {p2sc}')
+        return p2sc * rolled_times
+    elif p2sc >= 1000:
+        print(f'-- Player 2 won with {p2sc} points  > {p1sc}')
+        return p1sc * rolled_times
+    else:
+        return NotImplemented
 
 
 def main2(values) -> int:
@@ -65,7 +85,7 @@ if __name__ == '__main__':
 
     answ = main1(inp_values)
     print(f'\x1b[32;1mAnswer: {answ} \x1b[0m')
-    # Correct answer for my input:
+    # Correct answer for my input: 916083
 
     answ2 = main2(inp_values)
     print(f'Part 2,\x1b[32;1m Answer: {answ2} \x1b[0m')
