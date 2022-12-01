7c827e449596d3b2238ddfd4f1ac88c0f0579b0e
Jędrzej Pacanowski <42004734+jotpacanowski@users.noreply.github.com>
2022-12-01 14:09:00 +0100

    add day 1 Rust solution

---
+++ 2022/src/d1.rs
@@ -0,0 +1,63 @@
+use std::fs;
+
+const DAY: i32 = 1;
+const FN: &str = "in1.txt";
+
+fn parse_input(lines: Vec<&str>) -> Vec<Vec<i32>> {
+    let mut r: Vec<Vec<i32>> = Vec::with_capacity(20);
+    let mut st: Vec<i32> = vec![];
+
+    for l in lines {
+        if let Ok(i) = l.parse::<i32>() {
+            assert!(i > 0);
+            st.push(i);
+        } else {
+            r.push(st);
+            st = vec![];
+        }
+    }
+
+    r
+}
+
+fn solve1(elves: &Vec<Vec<i32>>) -> i32 {
+    elves.iter().map(|x| x.iter().sum()).max().unwrap()
+}
+
+fn solve1_simple(elves: &Vec<Vec<i32>>) -> i32 {
+    let mut max = 0;
+    for it in elves {
+        let s = it.iter().sum();
+        if s > max {
+            max = s;
+        }
+    }
+    max
+}
+
+fn solve2(elves: &Vec<Vec<i32>>) -> i32 {
+    let mut sums: Vec<i32> = elves.iter().map(|x| x.iter().sum()).collect();
+
+    // sums.sort();
+    sums.sort_unstable_by(|a, b| b.cmp(a));
+    // eprintln!("DBG: {sums:?}");
+    eprintln!("{:?}", &sums[..3]);
+
+    sums[..3].iter().sum()
+}
+
+fn main() {
+    println!("Advent of Code day {:02}!", DAY);
+
+    let input = fs::read_to_string(FN).expect("failed to read input file");
+    let lines: Vec<&str> = input.lines().collect();
+
+    println!("Read {} lines.", lines.len());
+
+    let elves = parse_input(lines);
+    println!("-> {} elves", elves.len());
+
+    println!("part1: {}", solve1(&elves));
+    println!("part1: {}", solve1_simple(&elves));
+    println!("part2: {}", solve2(&elves));
+}
