0d4504a856549e4ab33d786b5ed135588e36c9ca
Jędrzej Pacanowski <42004734+jotpacanowski@users.noreply.github.com>
2022-12-27 16:34:04 +0100

    add 2022 day 22 part 2

--- 2022/d22.py
+++ 2022/d22.py
@@ -77,7 +77,7 @@ def next_pos_common(board, current, dir):
 
 def next_pos_on_map(board, current, dir):
     v, ret = next_pos_common(board, current, dir)
-    if ret:
+    if ret is not None:
         return ret
     cr, cc = current
 
@@ -99,6 +99,84 @@ def next_pos_on_map(board, current, dir):
             ...
         valid = False
     return (lastok[0], lastok[1]), dir
+
+
+# hard-coded mapping that I computed with pen and paper
+EXAMPLE_LUT = {
+    (0, 2, 2): (1, lambda r, c: (4, 8 + (r - 4))),
+    (1, 1, 3): (2, lambda r, c: (4 - (8 - c), 8)),
+
+    (1, 2, 0): (1, lambda r, c: (8, 12 + (7 - r))),
+    (2, 3, 3): (2, lambda r, c: (7 - (c - 11), 11)),
+
+    (2, 2, 1): (3, lambda r, c: (7, 3 - (c - 8))),
+    # ...
+}
+MY_INPUT_LUT = {
+    (0, 1, 2): (0, lambda r, c: (100 + (49 - r), 0)),
+    (2, 0, 2): (0, lambda r, c: (49 - (r - 100), 50)),
+
+    (0, 1, 3): (0, lambda r, c: (150 + (c - 50), 0)),
+    (3, 0, 2): (1, lambda r, c: (0, 50 + (r - 150))),
+
+    (1, 1, 0): (3, lambda r, c: (49, 100 + (r - 50))),
+    (0, 2, 1): (2, lambda r, c: (50 + (c - 100), 99)),
+
+    (0, 2, 3): (3, lambda r, c: (199, c - 100)),
+    (3, 0, 1): (1, lambda r, c: (0, c + 100)),
+
+    (0, 2, 0): (2, lambda r, c: (100 + (49 - r), 99)),
+    (2, 1, 0): (2, lambda r, c: (49 - (r - 100), 149)),
+
+    (2, 0, 3): (0, lambda r, c: (99 - (49 - c), 50)),
+    (1, 1, 2): (1, lambda r, c: (100, 49 - (99 - r))),
+
+    (3, 0, 0): (3, lambda r, c: (149, 50 + (r - 150))),
+    (2, 1, 1): (2, lambda r, c: (150 + (c - 50), 49)),
+}
+cube_lookup = None
+cube_side = None
+
+
+def prepare_cube_lookup(board: np.ndarray, side):
+    global cube_lookup, cube_side
+    bmap_h = board.shape[0] / side
+    bmap_w = board.shape[1] / side
+    assert bmap_h.is_integer()
+    assert bmap_w.is_integer()
+    bmap_h = int(bmap_h)
+    bmap_w = int(bmap_w)
+
+    # bmap = np.zeros((bmap_h, bmap_w))
+    # for i in range(bmap_h):
+    #     for j in range(bmap_w):
+    #         if (board[side*i: side*i+side, side*j: side*j+side] != 0).any():
+    #             bmap[i, j] = 1
+    cube_side = side
+    if side == 4:
+        cube_lookup = EXAMPLE_LUT
+    else:
+        cube_lookup = MY_INPUT_LUT
+
+
+def next_pos_cube(board, current, dir):
+    v, ret = next_pos_common(board, current, dir)
+    if ret:
+        return ret
+    cr, cc = current
+
+    sider = (cr // cube_side) + ((cr % cube_side) // cube_side)
+    sidec = (cc // cube_side) + ((cc % cube_side) // cube_side)
+    # print(f'cur-row: {cr:3} side-row: {sider}')
+    # print(f'cur-col: {cc:3} side-col: {sidec}')
+
+    newd, func = cube_lookup[sider, sidec, dir]
+    nr, nc = func(cr, cc)
+
+    if board[nr, nc] == 2:
+        return 'wall'
+    else:
+        return (nr, nc), newd
 
 
 def solve(board: np.ndarray, moves: list[str | int], next_pos_func):
@@ -163,3 +241,17 @@ if __name__ == '__main__':
     r += 1
     c += 1
     print('part1:', 1000*r + 4*c + d)
+
+    print('\n')
+    prepare_cube_lookup(brd, 4 if inp.lstrip()[0] == 'e' else 50)
+    if inp == EXAMPLE:
+        # A to B
+        assert next_pos_cube(brd, (5, 11), 0) == ((8, 14), 1)
+        # C to D
+        print(next_pos_cube(brd, (11, 10), 1))
+        assert next_pos_cube(brd, (11, 10), 1) == ((7, 1), 3)
+    r, c, d = solve(brd, moves, next_pos_cube)
+    print('end', r, c, d)
+    r += 1
+    c += 1
+    print('part2:', 1000*r + 4*c + d)
